661f242b22e666b76d80502f2ead1a21bc2ee68c
wzc <newwangzhicheng@hotmail.com>
2021-12-22 17:08:11 +0800

    finish lab 9 part 1

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -1,5 +1,6 @@
 package lab9;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -25,7 +26,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         }
     }
 
-    private Node root;  /* Root node of the tree. */
+    private Node root; /* Root node of the tree. */
     private int size; /* The number of key-value pairs in the tree */
 
     /* Creates an empty BSTMap. */
@@ -40,70 +41,219 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         size = 0;
     }
 
-    /** Returns the value mapped to by KEY in the subtree rooted in P.
-     *  or null if this map contains no mapping for the key.
+    /**
+     * Returns the value mapped to by KEY in the subtree rooted in P.
+     * or null if this map contains no mapping for the key.
      */
     private V getHelper(K key, Node p) {
-        throw new UnsupportedOperationException();
+        if (p == null) {
+            return null;
+        } else if (key.compareTo(p.key) < 0) {
+            return getHelper(key, p.left);
+        } else if (key.compareTo(p.key) > 0) {
+            return getHelper(key, p.right);
+        }
+        return p.value;
     }
 
-    /** Returns the value to which the specified key is mapped, or null if this
-     *  map contains no mapping for the key.
+    /**
+     * Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        return getHelper(key, root);
     }
 
-    /** Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
-      * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
+    /**
+     * Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
+     * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
      */
     private Node putHelper(K key, V value, Node p) {
-        throw new UnsupportedOperationException();
+        if (p == null) {
+            return new Node(key, value);
+        }
+        Node parent = p;
+        while (true) {
+            if (key.compareTo(parent.key) < 0) {
+                if (parent.left == null) {
+                    parent.left = new Node(key, value);
+                    break;
+                } else {
+                    parent = parent.left;
+                }
+            } else if (key.compareTo(parent.key) > 0) {
+                if (parent.right == null) {
+                    parent.right = new Node(key, value);
+                    break;
+                } else {
+                    parent = parent.right;
+                }
+            }
+        }
+        return p;
     }
 
-    /** Inserts the key KEY
-     *  If it is already present, updates value to be VALUE.
+    /**
+     * Inserts the key KEY
+     * If it is already present, updates value to be VALUE.
      */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        if (get(key) == null) {
+            root = putHelper(key, value, root);
+            size++;
+        } else {
+            Node node = getNodeHelper(key, root);
+            node.value = value;
+        }
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
+    /**
+     * iterate BST with recursion
+     * 
+     * @param p
+     * @return
+     */
+    private void keySetHelper(Node p, Set<K> set) {
+        if (p != null) {
+            set.add(p.key);
+            keySetHelper(p.left, set);
+            keySetHelper(p.right, set);
+        }
+    }
 
     /* Returns a Set view of the keys contained in this map. */
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        Set<K> set = new HashSet<>();
+        keySetHelper(root, set);
+        return set;
     }
 
-    /** Removes KEY from the tree if present
-     *  returns VALUE removed,
-     *  null on failed removal.
+    /**
+     * get the parent node of p
+     * 
+     * @param key
+     * @param p
+     * @return
+     */
+    private Node getParentNodeHelper(K key, Node p) {
+        if ((p.left != null && p.left.key.compareTo(key) == 0) ||
+                (p.right != null && p.right.key.compareTo(key) == 0)) {
+            return p;
+        }
+        return null;
+    }
+
+    /**
+     * get the node of p
+     * 
+     * @param key
+     * @param p
+     * @return
+     */
+    private Node getNodeHelper(K key, Node p) {
+        if (p == null) {
+            return null;
+        } else if (key.compareTo(p.key) < 0) {
+            return getNodeHelper(key, p.left);
+        } else if (key.compareTo(p.key) > 0) {
+            return getNodeHelper(key, p.right);
+        }
+        return p;
+    }
+
+    /**
+     * Removes KEY from the tree if present
+     * returns VALUE removed,
+     * null on failed removal.
      */
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        /** key does not present */
+        if (get(key) == null) {
+            return null;
+        }
+
+        size--;
+        /** node to delete is root */
+        if (key.compareTo(root.key) == 0) {
+            V value = root.value;
+            root = null;
+            return value;
+        }
+
+        /** node to delete is not leaf */
+        Node replacedNode = getNodeHelper(key, root);
+        Node parentDeletedNode = replacedNode;
+        Node deletedNode = null;
+        if (replacedNode.left != null || replacedNode.right != null) {
+            if (replacedNode.left != null) {
+                /** find biggest node to replace deleted node in the left tree */
+                deletedNode = parentDeletedNode.left;
+                if (deletedNode.right == null) {
+                    parentDeletedNode.left = deletedNode.left;
+                } else {
+                    while (deletedNode.right != null) {
+                        parentDeletedNode = deletedNode;
+                        deletedNode = deletedNode.right;
+                    }
+                    parentDeletedNode.right = deletedNode.left;
+                }
+            } else {
+                /** find smallest node to replace deleted node in the left tree */
+                deletedNode = parentDeletedNode.right;
+                if (deletedNode.left == null) {
+                    parentDeletedNode.right = deletedNode.right;
+                } else {
+                    while (deletedNode.left != null) {
+                        parentDeletedNode = deletedNode;
+                        deletedNode = deletedNode.left;
+                    }
+                    parentDeletedNode.left = deletedNode.right;
+                }
+            }
+            V value = replacedNode.value;
+            replacedNode.key = deletedNode.key;
+            replacedNode.value = deletedNode.value;
+            return value;
+        }
+
+        /** node to delete is leaf */
+        parentDeletedNode = getParentNodeHelper(key, root);
+        V value = parentDeletedNode.value;
+        if (parentDeletedNode.left != null && parentDeletedNode.left.key.compareTo(key) == 0) {
+            parentDeletedNode.left = null;
+        } else {
+            parentDeletedNode.right = null;
+        }
+        return value;
+
     }
 
-    /** Removes the key-value entry for the specified key only if it is
-     *  currently mapped to the specified value.  Returns the VALUE removed,
-     *  null on failed removal.
+    /**
+     * Removes the key-value entry for the specified key only if it is
+     * currently mapped to the specified value. Returns the VALUE removed,
+     * null on failed removal.
      **/
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        if (get(key) != null && get(key).equals(value)) {
+            return remove(key);
+        }
+        return null;
     }
 
     @Override
     public Iterator<K> iterator() {
-        throw new UnsupportedOperationException();
+        return keySet().iterator();
     }
 }
--- lab9/lab9tester/TestBSTMap.java
+++ lab9/lab9tester/TestBSTMap.java
@@ -5,6 +5,9 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import lab9.BSTMap;
 
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * Tests by Brendan Hu, Spring 2015, revised for 2018 by Josh Hug
  */
@@ -22,13 +25,13 @@ public class TestBSTMap {
         }
     }
 
-    //assumes put/size/containsKey/get work
+    // assumes put/size/containsKey/get work
     @Test
     public void sanityClearTest() {
         BSTMap<String, Integer> b = new BSTMap<String, Integer>();
         for (int i = 0; i < 455; i++) {
             b.put("hi" + i, 1 + i);
-            //make sure put is working via containsKey and get
+            // make sure put is working via containsKey and get
             assertTrue(null != b.get("hi" + i));
             assertTrue(b.get("hi" + i).equals(1 + i));
             assertTrue(b.containsKey("hi" + i));
@@ -77,13 +80,74 @@ public class TestBSTMap {
         assertEquals(456, b.size());
     }
 
-    //assumes get/containskey work
+    // assumes get/containskey work
     @Test
     public void sanityPutTest() {
         BSTMap<String, Integer> b = new BSTMap<String, Integer>();
         b.put("hi", 1);
         assertTrue(b.containsKey("hi"));
         assertTrue(b.get("hi") != null);
+    }
+
+    @Test
+    public void sanityRemoveTest() {
+        BSTMap<String, Integer> b = new BSTMap<String, Integer>();
+        for (int i = 0; i < 455; i++) {
+            b.put("hi" + i, 1 + i);
+            // make sure put is working via containsKey and get
+            assertTrue(null != b.get("hi" + i));
+            assertTrue(b.get("hi" + i).equals(1 + i));
+            assertTrue(b.containsKey("hi" + i));
+        }
+        b.remove("hi1");
+        b.remove("hi10");
+        b.remove("hi2", 3);
+        b.remove("hi100", 102);
+        b.put("hi2", 222);
+        b.put("hi3", 333);
+        assertEquals(453, b.size());
+        assertEquals(null, b.get("hi1"));
+        assertEquals(null, b.get("hi10"));
+        assertEquals(Integer.valueOf(222), b.get("hi2"));
+        assertEquals(Integer.valueOf(101), b.get("hi100"));
+        assertEquals(Integer.valueOf(1), b.get("hi0"));
+        assertEquals(Integer.valueOf(333), b.get("hi3"));
+    }
+
+    @Test
+    public void sanityKeySetTest() {
+        BSTMap<String, Integer> b = new BSTMap<String, Integer>();
+        for (int i = 0; i < 12; i++) {
+            b.put("hi" + i, 1 + i);
+            // make sure put is working via containsKey and get
+            assertTrue(null != b.get("hi" + i));
+            assertTrue(b.get("hi" + i).equals(1 + i));
+            assertTrue(b.containsKey("hi" + i));
+        }
+        b.remove("hi1");
+        b.remove("hi10");
+        b.remove("hi2", 3);
+        b.remove("hi10", 102);
+        b.put("hi2", 222);
+        b.put("hi3", 333);
+        Set<String> keySet = b.keySet();
+        System.out.println(keySet);
+    }
+
+    @Test
+    public void sanityIteratorTest() {
+        BSTMap<String, Integer> b = new BSTMap<String, Integer>();
+        for (int i = 0; i < 12; i++) {
+            b.put("hi" + i, 1 + i);
+            // make sure put is working via containsKey and get
+            assertTrue(null != b.get("hi" + i));
+            assertTrue(b.get("hi" + i).equals(1 + i));
+            assertTrue(b.containsKey("hi" + i));
+        }
+        Iterator<String> keys = b.iterator();
+        while (keys.hasNext()) {
+            System.out.println(keys.next());
+        }
     }
 
     public static void main(String[] args) {
