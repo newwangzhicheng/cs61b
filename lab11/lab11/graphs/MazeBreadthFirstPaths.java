47b0e28ea7dad5967449f65683afab1e6510a3ff
wzc <newwangzhicheng@hotmail.com>
2022-01-06 17:45:01 +0800

    doing lab 11

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,29 +1,57 @@
 package lab11.graphs;
 
+import java.util.LinkedList;
+
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeBreadthFirstPaths extends MazeExplorer {
-    /* Inherits public fields:
-    public int[] distTo;
-    public int[] edgeTo;
-    public boolean[] marked;
-    */
+    /*
+     * Inherits public fields:
+     * public int[] distTo;
+     * public int[] edgeTo;
+     * public boolean[] marked;
+     */
+    private int s;
+    private int t;
+    private Maze maze;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
-    }
+        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as
+        // well as call announce()
+        LinkedList<Integer> queue = new LinkedList<>();
+        queue.offer(s);
+        while (!queue.isEmpty()) {
+            int out = queue.poll();
+            marked[out] = true;
+            announce();
+            if (out == t) {
+                return;
+            }
+            for (int w : maze.adj(out)) {
+                if (!marked[w]) {
+                    distTo[w] = distTo[out] + 1;
+                    edgeTo[w] = out;
+                    queue.offer(w);
+                }
+            }
+        }
 
+    }
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
-
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,14 +1,17 @@
 package lab11.graphs;
 
+import java.util.LinkedList;
+
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeCycles extends MazeExplorer {
-    /* Inherits public fields:
-    public int[] distTo;
-    public int[] edgeTo;
-    public boolean[] marked;
-    */
+    /*
+     * Inherits public fields:
+     * public int[] distTo;
+     * public int[] edgeTo;
+     * public boolean[] marked;
+     */
 
     public MazeCycles(Maze m) {
         super(m);
@@ -17,8 +20,37 @@ public class MazeCycles extends MazeExplorer {
     @Override
     public void solve() {
         // TODO: Your code here!
+        int s = 0;
+        int parent = s;
+        int[] copyEdgeTo = new int[maze.V()];
+        System.arraycopy(edgeTo, 0, copyEdgeTo, 0, maze.V());
+        LinkedList<Integer> stack = new LinkedList<>();
+        stack.addLast(s);
+        while (!stack.isEmpty()) {
+            int out = stack.removeLast();
+            marked[out] = true;
+            distTo[out] = out;
+            announce();
+            for (int w : maze.adj(out)) {
+                if (marked[w] && w != copyEdgeTo[out]) {
+                    int to = w;
+                    int from = out;
+                    do {
+                        edgeTo[to] = from;
+                        announce();
+                        to = from;
+                        from = copyEdgeTo[from];
+                    } while (to != w);
+                    return;
+                }
+                if (!marked[w]) {
+                    copyEdgeTo[w] = out;
+                    stack.addLast(w);
+                }
+            }
+            parent = out;
+        }
     }
 
     // Helper methods go here
 }
-
