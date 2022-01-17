ab4523200003882ee020e51ea42b6e8c2b28336e
wzc <newwangzhicheng@hotmail.com>
2022-01-17 09:49:24 +0800

    finish quicksort

--- lab12/MergeSort.java
+++ lab12/MergeSort.java
@@ -4,12 +4,13 @@ public class MergeSort {
     /**
      * Removes and returns the smallest item that is in q1 or q2.
      *
-     * The method assumes that both q1 and q2 are in sorted order, with the smallest item first. At
+     * The method assumes that both q1 and q2 are in sorted order, with the smallest
+     * item first. At
      * most one of q1 or q2 can be empty (but both cannot be empty).
      *
-     * @param   q1  A Queue in sorted order from least to greatest.
-     * @param   q2  A Queue in sorted order from least to greatest.
-     * @return      The smallest item that is in q1 or q2.
+     * @param q1 A Queue in sorted order from least to greatest.
+     * @param q2 A Queue in sorted order from least to greatest.
+     * @return The smallest item that is in q1 or q2.
      */
     private static <Item extends Comparable> Item getMin(
             Queue<Item> q1, Queue<Item> q2) {
@@ -32,11 +33,10 @@ public class MergeSort {
     }
 
     /** Returns a queue of queues that each contain one item from items. */
-    private static <Item extends Comparable> Queue<Queue<Item>>
-            makeSingleItemQueues(Queue<Item> items) {
+    private static <Item extends Comparable> Queue<Queue<Item>> makeSingleItemQueues(Queue<Item> items) {
         // Your code here!
         Queue<Queue<Item>> queues = new Queue<>();
-        while(!items.isEmpty()) {
+        while (!items.isEmpty()) {
             Queue<Item> singleQueue = new Queue<>();
             singleQueue.enqueue(items.dequeue());
             queues.enqueue(singleQueue);
@@ -47,27 +47,32 @@ public class MergeSort {
     /**
      * Returns a new queue that contains the items in q1 and q2 in sorted order.
      *
-     * This method should take time linear in the total number of items in q1 and q2.  After
-     * running this method, q1 and q2 will be empty, and all of their items will be in the
+     * This method should take time linear in the total number of items in q1 and
+     * q2. After
+     * running this method, q1 and q2 will be empty, and all of their items will be
+     * in the
      * returned queue.
      *
-     * @param   q1  A Queue in sorted order from least to greatest.
-     * @param   q2  A Queue in sorted order from least to greatest.
-     * @return      A Queue containing all of the q1 and q2 in sorted order, from least to
-     *              greatest.
+     * @param q1 A Queue in sorted order from least to greatest.
+     * @param q2 A Queue in sorted order from least to greatest.
+     * @return A Queue containing all of the q1 and q2 in sorted order, from least
+     *         to
+     *         greatest.
      *
      */
     private static <Item extends Comparable> Queue<Item> mergeSortedQueues(
             Queue<Item> q1, Queue<Item> q2) {
         // Your code here!
         Queue<Item> sortedQueue = new Queue<>();
-        while(!q1.isEmpty() || !q2.isEmpty()) {
+        while (!q1.isEmpty() || !q2.isEmpty()) {
             sortedQueue.enqueue(getMin(q1, q2));
         }
         return sortedQueue;
     }
 
-    /** Returns a Queue that contains the given items sorted from least to greatest. */
+    /**
+     * Returns a Queue that contains the given items sorted from least to greatest.
+     */
     public static <Item extends Comparable> Queue<Item> mergeSort(
             Queue<Item> items) {
         // Your code here!
@@ -78,7 +83,7 @@ public class MergeSort {
         Queue<Item> right = new Queue<>();
         int size = items.size();
         int i = 0;
-        while(!items.isEmpty()) {
+        while (!items.isEmpty()) {
             if (i < size / 2) {
                 left.enqueue(items.dequeue());
             } else {
@@ -94,12 +99,13 @@ public class MergeSort {
 
     public static void main(String[] args) {
         Queue<String> q = new Queue<>();
-        q.enqueue("Alice");
-        q.enqueue("Ethan");
-        q.enqueue("Vanessa");
+        q.enqueue("a");
+        q.enqueue("c");
+        q.enqueue("d");
+        q.enqueue("b");
 
         Queue<String> sorted = MergeSort.mergeSort(q);
-        while(!sorted.isEmpty()) {
+        while (!sorted.isEmpty()) {
             System.out.println(sorted.dequeue());
         }
     }
--- lab12/QuickSort.java
+++ lab12/QuickSort.java
@@ -11,7 +11,7 @@ public class QuickSort {
         for (Item item : q1) {
             catenated.enqueue(item);
         }
-        for (Item item: q2) {
+        for (Item item : q2) {
             catenated.enqueue(item);
         }
         return catenated;
@@ -35,25 +35,69 @@ public class QuickSort {
     /**
      * Partitions the given unsorted queue by pivoting on the given item.
      *
-     * @param unsorted  A Queue of unsorted items
-     * @param pivot     The item to pivot on
-     * @param less      An empty Queue. When the function completes, this queue will contain
-     *                  all of the items in unsorted that are less than the given pivot.
-     * @param equal     An empty Queue. When the function completes, this queue will contain
-     *                  all of the items in unsorted that are equal to the given pivot.
-     * @param greater   An empty Queue. When the function completes, this queue will contain
-     *                  all of the items in unsorted that are greater than the given pivot.
+     * @param unsorted A Queue of unsorted items
+     * @param pivot    The item to pivot on
+     * @param less     An empty Queue. When the function completes, this queue will
+     *                 contain
+     *                 all of the items in unsorted that are less than the given
+     *                 pivot.
+     * @param equal    An empty Queue. When the function completes, this queue will
+     *                 contain
+     *                 all of the items in unsorted that are equal to the given
+     *                 pivot.
+     * @param greater  An empty Queue. When the function completes, this queue will
+     *                 contain
+     *                 all of the items in unsorted that are greater than the given
+     *                 pivot.
      */
     private static <Item extends Comparable> void partition(
             Queue<Item> unsorted, Item pivot,
             Queue<Item> less, Queue<Item> equal, Queue<Item> greater) {
         // Your code here!
+        while (!unsorted.isEmpty()) {
+            Item item = unsorted.dequeue();
+            if (item.compareTo(pivot) < 0) {
+                less.enqueue(item);
+            } else if (item.compareTo(pivot) > 0) {
+                greater.enqueue(item);
+            } else {
+                equal.enqueue(item);
+            }
+        }
     }
 
-    /** Returns a Queue that contains the given items sorted from least to greatest. */
+    /**
+     * Returns a Queue that contains the given items sorted from least to greatest.
+     */
     public static <Item extends Comparable> Queue<Item> quickSort(
             Queue<Item> items) {
         // Your code here!
-        return items;
+        if (items.size() <= 1) {
+            return items;
+        }
+        Queue<Item> less = new Queue<>();
+        Queue<Item> equal = new Queue<>();
+        Queue<Item> greater = new Queue<>();
+        Item pivot = getRandomItem(items);
+        partition(items, pivot, less, equal, greater);
+        less = quickSort(less);
+        greater = quickSort(greater);
+        Queue<Item> sorted = catenate(less, equal);
+        sorted = catenate(sorted, greater);
+        return sorted;
+    }
+
+    public static void main(String[] args) {
+        Queue<String> q = new Queue<>();
+        q.enqueue("d");
+        q.enqueue("a");
+        q.enqueue("c");
+        q.enqueue("b");
+
+        Queue<String> sorted = QuickSort.quickSort(q);
+        while (!sorted.isEmpty()) {
+            System.out.println(sorted.dequeue());
+        }
+
     }
 }
